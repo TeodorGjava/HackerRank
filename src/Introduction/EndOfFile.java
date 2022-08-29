fc516892626bdcf84e702f5a8f1ca0393a589ac6
Teodor Georgiev <tgeorgievmk2@gmail.com>
2022-08-29 13:53:16 +0300

    Create EndOfFile.java

---
+++ src/Introduction/EndOfFile.java
@@ -0,0 +1,23 @@
+package Introduction;
+
+import java.util.Scanner;
+
+public class EndOfFile {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int count = 0;
+        while (sc.hasNext()) {
+            String input = sc.nextLine();
+            while (!input.equals("Read me until end-of-file.")) {
+                count++;
+                System.out.println(count + " " + input);
+                input = sc.nextLine();
+                if (input.equals("Read me until end-of-file.")) {
+                    count++;
+                    System.out.println(count + " " + input);
+                    break;
+                }
+            }
+        }
+    }
+}
