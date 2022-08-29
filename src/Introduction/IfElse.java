ede99e6c075d4cbeae3fa4a3cc6deb62ca6be245
Teodor Georgiev <tgeorgievmk2@gmail.com>
2022-08-29 13:53:26 +0300

    Update IfElse.java

--- src/Introduction/IfElse.java
+++ src/Introduction/IfElse.java
@@ -3,10 +3,10 @@ package Introduction;
 import java.util.Scanner;
 
 public class IfElse {
-    private static final Scanner scanner = new Scanner(System.in);
+    private static final Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-        int N = scanner.nextInt();
+        int N = sc.nextInt();
         if (N % 2 != 0) {
             System.out.println("Weird");
         } else if (N >= 2 && N <= 5) {
@@ -16,8 +16,8 @@ public class IfElse {
         } else if (N > 20) {
             System.out.println("Not Weird");
         }
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        sc.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
-        scanner.close();
+        sc.close();
     }
 }
