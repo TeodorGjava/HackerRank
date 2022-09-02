8c1f0fb59d5ce17238d00d1d740dd6bd77f03f73
Teodor Georgiev <tgeorgievmk2@gmail.com>
2022-09-02 14:27:22 +0300

    Update CollectionSortBigDecimal.java

--- src/BigInteger/CollectionSortBigDecimal.java
+++ src/BigInteger/CollectionSortBigDecimal.java
@@ -1,33 +1,33 @@
 package BigInteger;
 
 import java.math.BigDecimal;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class CollectionSortBigDecimal {
-    public static void main(String[] args) {
-
-
-    Scanner sc= new Scanner(System.in);
-    int n=sc.nextInt();
-    String []s=new String[n+2];
+    public static void main(String []args){
+        //Input
+        Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt();
+        String []s=new String[n+2];
         for(int i=0;i<n;i++){
-        s[i]=sc.next();
-    }
+            s[i]=sc.next();
+        }
         sc.close();
 
-    List<BigDecimal> decimals = Arrays.stream(s).map(str->new BigDecimal(String.valueOf(BigDecimal.valueOf(Double.parseDouble(str))))).sorted().collect(Collectors.toList());
-        Collections.reverse(decimals);
-     for (int i = 0; i < s.length; i++) {
-        s[i] = String.valueOf(decimals.get(i));
-    }
-    //Output
+        for (int i = 0; i < s.length-2; i++) {
+            for (int j = 1; j < s.length-2; j++) {
+                if (Double.parseDouble(s[j-1])<Double.parseDouble(s[j])) {
+                    String temp = s[j-1];
+                    s[j-1] = s[j];
+                    s[j] = temp;
+                }
+            }
+        }
+        //Output
         for(int i=0;i<n;i++)
-    {
-        System.out.println(s[i]);
+        {
+            System.out.println(s[i]);
+        }
     }
-}
 }
