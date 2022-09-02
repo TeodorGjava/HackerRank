62b7e7aaa259ef3416a11aac813edc1770e155c7
Teodor Georgiev <tgeorgievmk2@gmail.com>
2022-09-02 09:36:39 +0300

    Create Operations.java

---
+++ src/BigInteger/Operations.java
@@ -0,0 +1,19 @@
+package BigInteger;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Operations {
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        BigInteger a = new BigInteger(sc.nextLine());
+        BigInteger b = new BigInteger(sc.nextLine());
+
+        BigInteger sum = a.add(b);
+        BigInteger multi = a.multiply(b);
+
+        System.out.println(sum);
+        System.out.println(multi);
+    }
+}
