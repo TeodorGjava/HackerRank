febf3d77dd2cc237fcca16959bb9f219caa8152d
TeodorGjava <90547780+TeodorGjava@users.noreply.github.com>
2022-09-04 09:29:39 +0300

    Create TwoDimensionalArray.java

---
+++ src/DataStructures/TwoDimensionalArray.java
@@ -0,0 +1,22 @@
+package DataStructures;
+
+import java.util.Scanner;
+
+public class TwoDimensionalArray {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            int x = scan.nextInt();
+            a[i] = x;
+        }
+        scan.close();
+
+        // Prints each sequential element in array a
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+    }
+}
